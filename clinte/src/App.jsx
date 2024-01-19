4552789a552ace1ebb943468400fa93dcf161eb0
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-01-19 19:01:14 +0200

    Install react-router-dom with basic routing

--- clinte/src/App.jsx
+++ clinte/src/App.jsx
@@ -1,8 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import NotFound from './pages/NotFound';
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl text-blue-600">MERN Blog</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
