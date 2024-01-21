12f2eafe5b789c88a9cb652e145efb33762391c3
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-01-21 09:34:28 +0200

    Create signup route

--- api/index.js
+++ api/index.js
@@ -1,24 +1,27 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from '../api/routes/user.route.js';
-
-dotenv.config();
-
-mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+import userRoutes from './routes/user.route.js';
+import aouthRoutes from './routes/aouth.route.js';
 
 const app = express();
-const PORT = 3000 || process.env.PORT;
+app.use(express.json());
+dotenv.config();
 
-app.use('/api/user', userRoutes);
+try {
+  mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB');
+  });
+} catch (err) {
+  console.log(err);
+}
+
+const PORT = 3000 || process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT} http://localhost:3000`);
 });
+
+app.use('/api/user', userRoutes);
+
+app.use('/api/aouth', aouthRoutes);
